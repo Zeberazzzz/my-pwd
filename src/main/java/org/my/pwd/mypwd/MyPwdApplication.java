29344de82a0d44868728aec1a0e85a77dc766c6c
SHIB <fooson2023@gmail.com>
2024-03-25 00:07:08 +0800

    导入项目

---
+++ src/main/java/org/my/pwd/mypwd/MyPwdApplication.java
@@ -0,0 +1,13 @@
+package org.my.pwd.mypwd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPwdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyPwdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/my/pwd/mypwd/MyPwdApplicationTests.java
@@ -0,0 +1,13 @@
+package org.my.pwd.mypwd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyPwdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
